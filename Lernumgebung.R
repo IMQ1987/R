43d7b6ce60874dfbb0394a5718ad010238e5a516
IMQ1987 <malte.quast@gmail.com>
2018-01-02 15:57:27 +0100

    First Commit

---
+++ Lernumgebung.R
@@ -0,0 +1,45 @@
+library(tidyverse)
+#library(dplyr) part of tidyverse
+library(rvest)
+library(ggplot2)
+
+'
+ggplot2
+dplyr
+lubridate
+tidyr
+stringr
+readr
+forcats
+'
+
+getwd()
+setwd("/home/malte/Schreibtisch/Lernumgebung")
+data <- read.csv2("SQL - Customer.csv", sep = ",", header = F)
+names(data) <- c("Index","Firma","Name", "Straße", "Stadt", "PLZ", "Land")
+
+length(data$Land[data$Land == "Germany"])
+
+
+bspmatrix<-matrix(1:30,byrow=T,ncol=10)
+apply(bspmatrix,2,sum)
+
+colnames()
+
+#dplyr
+data %>%
+  filter(Land == "Germany")
+
+################################## RESEARCH TOPICS ###########################
+summary
+table
+gsub #für string ersetzen
+by
+with()
+head
+floor
+library(reshape)
+cut
+subset
+grid
+
